6554b59628bd8383f4bdb8d0bdf2fd6f833e6ba4
User <User@Admin>
2024-10-25 19:49:02 +0530

    VotingException,Polymorphism

---
+++ MavenEx3/src/main/java/polymorphism/PolymorphismEx.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+public class PolymorphismEx 
+{
+	public void display(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+}
---
+++ MavenEx3/src/main/java/polymorphism/Polymorphismchild.java
@@ -0,0 +1,21 @@
+package polymorphism;
+
+public class Polymorphismchild extends PolymorphismEx 
+{
+
+	public void display(int a, int b)
+	{
+		int c=a-b;
+		System.out.println(c);
+		super.display(45,67);
+	}
+public static void main(String[] args) 
+{
+	// TODO Auto-generated method stub
+	Polymorphismchild obj=new Polymorphismchild();
+	obj.display(34, 45);
+	
+} 
+
+
+}
